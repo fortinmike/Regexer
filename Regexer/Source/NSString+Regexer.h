1227ed3b9fed1202fda218ea1b1c20453898854e
Michaël Fortin <fortinmike@irradiated.net>
2014-05-02 19:08:59 -0400

    Project initial setup and folder structure cleanup.

---
+++ Regexer/Source/NSString+Regexer.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Regexer.h
+//  Regexer
+//
+//  Created by Michaël Fortin on 2014-05-02.
+//  Copyright (c) 2014 Michaël Fortin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Regexer)
+
+@end
---
+++ Regexer/Source/NSString+Regexer.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Regexer.m
+//  Regexer
+//
+//  Created by Michaël Fortin on 2014-05-02.
+//  Copyright (c) 2014 Michaël Fortin. All rights reserved.
+//
+
+#import "NSString+Regexer.h"
+
+@implementation NSString (Regexer)
+
+@end
